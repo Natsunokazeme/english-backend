86157e6bedd49e5fe63bbe5c63e75de17467dc7f
Jeff <jeff.luo@henkel.com>
2023-08-08 17:40:41 +0800

    feat(): use localhost(8.8)

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { WordModule } from './word/word.module';
     WordModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'host.docker.internal',
+      host: 'localhost',
       port: 3306,
       username: 'root',
       password: 'admin',
